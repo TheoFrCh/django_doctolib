32c1f54d89559a7fe8b0c87e08f37ec3ae15fd31
Steve <stevebotella@hotmail.fr>
2021-12-01 13:55:36 +0100

    feat: table qui fonctionnent apparemment

--- doctodjango/doctor/models.py
+++ doctodjango/doctor/models.py
@@ -1,18 +1,14 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
 class Doctor(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE, )
-    name = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
 
 
 class Patient(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE, )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
 
 
